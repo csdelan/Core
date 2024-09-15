0f5085927e3738947f681b5bb8823af3f98eee1e
Chris Delaney <csdelan@outlook.com>
2024-09-15 10:19:28 -0700

    finished fixing up new repo Core

---
+++ Core.Audio/AudioManager.cs
@@ -0,0 +1,69 @@
+﻿using Serilog;
+using System.ComponentModel;
+
+namespace Common
+{
+    public static class AudioManager
+    {
+        private static Queue<AudioTrack> sequentialSoundPlayers = new Queue<AudioTrack>();
+        private const int _SleepDelay = 200;
+        private static AudioTrack StreamingAudio;
+        private static BackgroundWorker sequentialQueuePlayer = new BackgroundWorker();
+
+        static AudioManager()
+        {
+            StreamingAudio = new AudioTrack();
+            sequentialQueuePlayer.DoWork += SequentialQueuePlayer_DoWork;
+            sequentialQueuePlayer.WorkerSupportsCancellation = true;
+            sequentialQueuePlayer.RunWorkerAsync();
+        }
+
+        private static void SequentialQueuePlayer_DoWork(object? sender, DoWorkEventArgs e)
+        {
+            while (!e.Cancel)
+            {
+                if (sequentialSoundPlayers.TryDequeue(out AudioTrack trackPlayer) == true)
+                {
+                    Log.Information("Playing {0}", trackPlayer.Uri);
+                    trackPlayer.Play();
+                    while (!trackPlayer.IsDone)
+                        Thread.Sleep(_SleepDelay);
+                }
+                Thread.Sleep(_SleepDelay);
+            }
+        }
+
+        public static void BeginStreamingAudio(Uri uri)
+        {
+            StreamingAudio.Play(uri);
+        }
+
+        public static void StopStreamingAudio()
+        {
+            StreamingAudio.Stop();
+        }
+
+        public static void PauseStreamingAudio()
+        {
+            StreamingAudio.Pause();
+        }
+
+        public static void ResumeStreamingAudio()
+        {
+            StreamingAudio.Resume();
+        }
+
+        public static void BeginPlaySound(Uri uri, bool overlapAudio = false)
+        {
+            if (!overlapAudio)
+            {
+                AudioTrack newSeqTrack = new AudioTrack(uri);
+                sequentialSoundPlayers.Enqueue(newSeqTrack);
+                return;
+            }
+
+            AudioTrack newTrack = new AudioTrack();
+            newTrack.Play(uri);
+        }
+    }
+}
---
+++ Core.Audio/AudioTrack.cs
@@ -0,0 +1,64 @@
+﻿using Serilog;
+
+namespace Common
+{
+    internal class AudioTrack
+    {
+        public Uri? Uri { get; set; }
+
+        private MediaPlayer.MediaPlayer mediaPlayer = new MediaPlayer.MediaPlayer();
+
+        public AudioTrack(Uri location)
+        {
+            Uri = location;
+        }
+
+        public AudioTrack()
+        {
+        }
+
+        public void Play(Uri uri)
+        {
+            mediaPlayer.AutoStart = false;
+            mediaPlayer.FileName = uri.LocalPath;
+            mediaPlayer.Play();
+        }
+
+        public void Play()
+        {
+            Play(Uri);
+        }
+
+        public void Stop()
+        {
+            mediaPlayer.Stop();
+        }
+
+        public void Pause()
+        {
+            mediaPlayer.Pause();
+        }
+
+        public void Resume()
+        {
+            mediaPlayer.Play();
+        }
+
+        public bool IsDone
+        {
+            get
+            {
+                try
+                {
+                    var currentState = mediaPlayer.PlayState;
+                    Log.Information(currentState.ToString());
+                    var desiredState1 = MediaPlayer.MPPlayStateConstants.mpClosed;
+                    var desiredState2 = MediaPlayer.MPPlayStateConstants.mpStopped;
+                    return (mediaPlayer.PlayState == desiredState1 ||
+                            mediaPlayer.PlayState == desiredState2);
+                }
+                catch { return false; }
+            }
+        }
+    }
+}
