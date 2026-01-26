e75d88dedcddf02f8f8bcb5d287eeab01c701f25
Chris Delaney <csdelan@outlook.com>
2026-01-25 19:12:50 -0500

    Licensing helper

---
+++ Core/SyncfusionLicenser.cs
@@ -0,0 +1,24 @@
+﻿using Serilog;
+using Syncfusion.Licensing;
+
+namespace Core
+{
+    static internal class SyncfusionLicenser
+    {
+        static internal void RegisterLicense()
+        {
+            // Read Environment Variable for Syncfusion license key
+            var licenseKey = Environment.GetEnvironmentVariable("SYNCFUSIONKEY_27_2_2");
+            if (licenseKey != null)
+            {
+                // Syncfusion license registration
+                SyncfusionLicenseProvider.RegisterLicense(licenseKey);
+                Log.Debug("Syncfusion license registered from Environment Variable");
+                return;
+            }
+
+            // Syncfusion license registration
+            Log.Debug("Syncfusion license not found in environment variable: SYNCFUSIONKEY_27_2_2");
+        }
+    }
+}
